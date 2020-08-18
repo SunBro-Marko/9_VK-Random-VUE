da40788a18ae77aee16326132e0927ac3db4e500
Marko SunBro <sunbro.marko@yandex.ru>
2020-08-19 01:12:22 +0300

    add api for test

--- server/routes/ruffle.routes.js
+++ server/routes/ruffle.routes.js
@@ -1,10 +1,26 @@
 const {Router} = require('express')
 const router = Router()
 
+
+var winners = [
+    {name:"Пётр Петрович",img_url:"https://via.placeholder.com/150x300",isonline:true,isfollower:true,isopened:true},
+    {name:"Иван Иванович",img_url:"https://via.placeholder.com/150x300",isonline:true,isfollower:true,isopened:true},
+    {name:"Бородач Бородович",img_url:"https://via.placeholder.com/150x300",isonline:true,isfollower:true,isopened:true}
+]
+
 // /api/auth/
-router.post('/',async (req,res)=>{
+router.get('/',async (req,res)=>{
     try{
 
+    }
+    catch(e){
+
+    }
+})
+
+router.get('/winners',async (req,res)=>{
+    try{
+        res.send({winners});
     }
     catch(e){
 
