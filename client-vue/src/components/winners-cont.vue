855037933dfb899a12c575a51b21e6cf0cb89106
Marko SunBro <sunbro.marko@yandex.ru>
2020-08-19 00:27:15 +0300

    Vue-app update

---
+++ client-vue/src/components/winners-cont.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <winner v-bind:winner="user" />
+  </div>
+</template>
+
+<script>
+import winner from "@/components/winner.vue";
+export default {
+  name: "Home",
+  components: {
+    winner,
+  },
+  data() {
+    return {
+      user: {
+        name: "Дядя Федор",
+        img_url:
+          "https://avatars.mds.yandex.net/get-pdb/1365420/53f87cbb-ceb7-45d8-a7aa-22ba74c12167/s1200?webp=false",
+        isonline: true,
+        isopened: true,
+        isfollower: true,
+      },
+    };
+  },
+};
+</script>
+
+<style></style>
--- client-vue/src/views/Home.vue
+++ client-vue/src/views/Home.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <winnercont/>    
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import winnercont from "@/components/winners-cont.vue";
 
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
-    HelloWorld
-  }
-}
+    winnercont,
+  },
+};
 </script>
