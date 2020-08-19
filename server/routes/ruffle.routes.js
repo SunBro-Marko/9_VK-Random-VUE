ece937d38ec06b3fd620185e9c86a17e57fd5d61
Marko SunBro <sunbro.marko@yandex.ru>
2020-08-19 03:23:10 +0300

    CORS setting for test api

--- server/routes/ruffle.routes.js
+++ server/routes/ruffle.routes.js
@@ -3,9 +3,9 @@ const router = Router()
 
 
 var winners = [
-    {name:"Пётр Петрович",img_url:"https://via.placeholder.com/150x300",isonline:true,isfollower:true,isopened:true},
-    {name:"Иван Иванович",img_url:"https://via.placeholder.com/150x300",isonline:true,isfollower:true,isopened:true},
-    {name:"Бородач Бородович",img_url:"https://via.placeholder.com/150x300",isonline:true,isfollower:true,isopened:true}
+    {name:"Пётр Петрович",img_url:"https://via.placeholder.com/150x200",isonline:true,isfollower:true,isopened:true},
+    {name:"Иван Иванович",img_url:"https://via.placeholder.com/150x200",isonline:true,isfollower:true,isopened:true},
+    {name:"Бородач Бородович",img_url:"https://via.placeholder.com/150x200",isonline:true,isfollower:true,isopened:true}
 ]
 
 // /api/auth/
@@ -18,9 +18,11 @@ router.get('/',async (req,res)=>{
     }
 })
 
-router.get('/winners',async (req,res)=>{
+router.get("/winners",async (req,res)=>{
     try{
-        res.send({winners});
+        console.log("Сюды стучат")
+        res.set('Access-Control-Allow-Origin','*')
+        res.send(winners);
     }
     catch(e){
 
